b9c855b54994e00ab6ffd9a3414e36dbb76b2b03
Matt Bolger <mpbolger2@gmail.com>
2025-08-05 21:26:15 -0400

    StateMachine & Temp Skeleton
    Started the enemy state machine
    
    Imported a temp skeleton asset to prototype with

---
+++ Assets/Scripts/StateMachine/BaseState.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System;
+
+public abstract class BaseState<EState> where EState : Enum
+{
+    public BaseState(EState key)
+    {
+        StateKey = key;
+    }
+
+    public EState StateKey { get; private set; }
+
+    public abstract void EnterState();
+    public abstract void ExitState();
+    public abstract void UpdateState();
+    public abstract EState GetNextState();
+}
