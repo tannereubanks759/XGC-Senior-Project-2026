757b92f2dae69d875fdc2d3883a3fb3b63654b63
Andrewk57 <112024432+Andrewk57@users.noreply.github.com>
2025-08-07 23:04:16 -0400

    inv

--- Assets/Scripts/inventoryScript.cs
+++ Assets/Scripts/inventoryScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class inventoryScript : MonoBehaviour
 {
     public List<GameObject> items = new List<GameObject>();
+    public GameObject inventoryUI;
+    public bool isOpen = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,11 +16,30 @@ public class inventoryScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Tab)) 
+        {
+            inventoryUI.SetActive(!isOpen);
+            isOpen = !isOpen;
+        }
     }
     public void addToInventory(GameObject itemToAdd)
     {
-        Debug.Log("Adding", itemToAdd);
+        //Debug.Log("Adding", itemToAdd);
         items.Add(itemToAdd);
+    }
+    public void removeInventory(GameObject itemToRemove) 
+    {
+        items.Remove(itemToRemove);
+    }
+    public void updateUI(GameObject itemBeingUpdated, bool isAdd)
+    {
+        if(isAdd)
+        {
+            //logic for adding to ui
+        }
+        else
+        {
+            // logic for removing from ui
+        }
     }
 }
