65d40da031d52dc373d63f201bd3e9351aaf77cf
tannereubanks759 <112025050+tannereubanks759@users.noreply.github.com>
2025-09-16 17:45:28 -0400

    commit reload

--- Assets/Scripts/Blunderbuss.cs
+++ Assets/Scripts/Blunderbuss.cs
@@ -72,14 +72,17 @@ public class Blunderbuss : MonoBehaviour
                 //ShowShotLine(BulletPos.transform.position, hit.point);
 
                 Destroy(Instantiate(PelletHitEffect, hit.point, Quaternion.FromToRotation(transform.up, hit.normal)), 3f);
-                /*
+                if(hit.collider.tag == "Enemy")
+                {
+                    hit.collider.gameObject.GetComponent<GruntEnemyAI>().TakeDamage(DamagePerPellet);
+                }
                 // Damage what was hit
-                IDamageable damageable = hit.collider.GetComponent<IDamageable>();
+                /*IDamageable damageable = hit.collider.GetComponent<IDamageable>();
                 if (damageable != null)
                 {
                     damageable.TakeDamage(10);
-                }
-                */
+                }*/
+                
             }
         }
 
