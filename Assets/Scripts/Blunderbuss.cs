bb7178f79eea64244b9ebac6378f9e056ef8b8f0
tannereubanks759 <112025050+tannereubanks759@users.noreply.github.com>
2025-09-20 16:03:57 -0400

    Gun kills enemies

--- Assets/Scripts/Blunderbuss.cs
+++ Assets/Scripts/Blunderbuss.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class Blunderbuss : MonoBehaviour
 {
@@ -51,49 +53,69 @@ public class Blunderbuss : MonoBehaviour
         }
     }
 
-    void Shoot()
+void Shoot()
+{
+    wIntertia.FireRecoil(2);
+    Destroy(Instantiate(MuzzleFlashParticle, BulletPos.transform.position, BulletPos.transform.rotation), 3f);
+    isLoaded = false;
+    anim.SetBool("canShoot", false);
+    totalAmmo--;
+
+    //spread based on your reticle
+    bulletRadius = ret.GetComponent<RectTransform>().anchoredPosition.x / 10f;
+
+    //Collect pellet hits by target
+    var hitsByTarget = new Dictionary<GameObject, int>();
+
+    for (int i = 0; i < PelletPerBullet; i++)
     {
-        wIntertia.FireRecoil(2);
-        Destroy(Instantiate(MuzzleFlashParticle, BulletPos.transform.position, BulletPos.transform.rotation), 3f);
-        isLoaded = false;
-        anim.SetBool("canShoot", false);
-        totalAmmo--;
-        bulletRadius = ret.GetComponent<RectTransform>().anchoredPosition.x / 10;
-        //Debug.Log(bulletRadius);
-        for (int i = 0; i < PelletPerBullet; i++)
+        float randomx = Random.Range(-bulletRadius, bulletRadius);
+        float randomy = Random.Range(-bulletRadius, bulletRadius);
+        Vector3 screenPos = Input.mousePosition + new Vector3(randomx, randomy, 0f);
+        Ray ray = Camera.main.ScreenPointToRay(screenPos);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layers))
         {
-            float randomx = Random.Range(-bulletRadius, bulletRadius);
-            float randomy = Random.Range(-bulletRadius, bulletRadius);
-            Vector3 ScreenPos = Input.mousePosition + new Vector3(randomx, randomy, 0);
-            Ray Ray = Camera.main.ScreenPointToRay(ScreenPos);
-            
-            if (Physics.Raycast(Ray, out RaycastHit hit, Mathf.Infinity, layers))
+            Destroy(Instantiate(PelletHitEffect, hit.point, Quaternion.FromToRotation(transform.up, hit.normal)), 3f);
+
+            //Find the canonical GameObject for this target (rigidbody root or transform root)
+            GameObject targetGO = hit.rigidbody ? hit.rigidbody.gameObject : hit.transform.root.gameObject;
+
+            //Only count things we intend to damage
+            if (targetGO.CompareTag("Enemy") || targetGO.CompareTag("Skull"))
             {
-                //ShowShotLine(BulletPos.transform.position, hit.point);
-
-                Destroy(Instantiate(PelletHitEffect, hit.point, Quaternion.FromToRotation(transform.up, hit.normal)), 3f);
-                if (hit.collider.tag == "Enemy")
-                {
-                    hit.collider.gameObject.GetComponent<GruntEnemyAI>().TakeDamage(DamagePerPellet);
-                }
-                else if (hit.collider.tag == "Skull")
-                {
-                    hit.collider.gameObject.GetComponent<FloatingSkullAI>().ApplyDamage(DamagePerPellet);
-                }
-                // Damage what was hit
-                /*IDamageable damageable = hit.collider.GetComponent<IDamageable>();
-                if (damageable != null)
-                {
-                    damageable.TakeDamage(10);
-                }*/
-                
+                if (hitsByTarget.TryGetValue(targetGO, out int count))
+                    hitsByTarget[targetGO] = count + 1;
+                else
+                    hitsByTarget[targetGO] = 1;
             }
-        }
 
-        
-        
+            //(Optional) draw tracers etc.
+            //ShowShotLine(BulletPos.transform.position, hit.point);
+        }
     }
-    void SetLoaded()
+
+    //Apply a single damage call per target (hits * DamagePerPellet)
+    foreach (var kvp in hitsByTarget)
+    {
+        GameObject target = kvp.Key;
+        int hits = kvp.Value;
+        int totalDamage = hits * DamagePerPellet;
+
+        if (target.CompareTag("Enemy"))
+        {
+            var grunt = target.GetComponentInParent<GruntEnemyAI>();
+            if (grunt != null) grunt.TakeDamage(totalDamage);
+        }
+        else if (target.CompareTag("Skull"))
+        {
+            var skull = target.GetComponentInParent<FloatingSkullAI>();
+            if (skull != null) skull.ApplyDamage(totalDamage);
+        }
+    }
+}
+
+void SetLoaded()
     {
         isLoaded = true;
         anim.SetBool("canShoot", true);
