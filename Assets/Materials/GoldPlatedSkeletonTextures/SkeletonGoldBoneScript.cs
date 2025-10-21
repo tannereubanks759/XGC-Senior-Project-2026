a23b40e65ee7cbe242a98c6234a772bd8bef0386
tannereubanks759 <112025050+tannereubanks759@users.noreply.github.com>
2025-10-20 20:36:56 -0400

    Golden Bones

---
+++ Assets/Materials/GoldPlatedSkeletonTextures/SkeletonGoldBoneScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SkeletonGoldBoneScript : MonoBehaviour
+{
+    private Renderer rend;
+    private Material mat;
+    int goldBoneCount = 0;
+    public Texture2D[] masks;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        mat = rend.material;
+
+        goldBoneCount = Random.Range(0, 4);
+        Debug.Log(goldBoneCount);
+        for(int i = 0; i < goldBoneCount+1; i++)
+        {
+            if(i != 0)
+            {
+                mat.SetTexture("mask" + i.ToString(), masks[Random.Range(0, masks.Length - 1)]);
+            }
+        }
+    }
+
+    
+}
