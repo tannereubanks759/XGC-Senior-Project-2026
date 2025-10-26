abaca5ee0b61dd9c9ce39a0e2cd70cd3afa96ba2
tannereubanks759 <112025050+tannereubanks759@users.noreply.github.com>
2025-10-26 16:07:07 -0400

    skeleton mask updated

---
+++ Assets/Scripts/IslandTeleporter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class IslandTeleporter : MonoBehaviour
+{
+    public Animation doorAnim;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
