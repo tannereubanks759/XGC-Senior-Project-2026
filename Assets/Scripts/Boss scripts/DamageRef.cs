9ce241c04d22aa21a4bad49fdd58d49dc5505743
tannereubanks759 <112025050+tannereubanks759@users.noreply.github.com>
2025-11-29 01:24:24 -0500

    Bosses Drop Gold

--- Assets/Scripts/Boss scripts/DamageRef.cs
+++ Assets/Scripts/Boss scripts/DamageRef.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class DamageRef : MonoBehaviour
 {
     private PirateBossAI anchorBoss;
-    private MagmaBossAI magmaBoss; 
+    private MagmaBossAI magmaBoss;
+
+    public GameObject GoldBagPrefab;
+    public int OverrideGoldAmount = 0; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,13 +16,54 @@ public class DamageRef : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
+        SpawnGoldIfDead((int)damage);
+
         if (anchorBoss)
         {
+            
             anchorBoss.TakeDamage((int) damage);
+            
         }
         if (magmaBoss)
         {
+            
             magmaBoss.TakeDamage((int)damage);
+        }
+    }
+
+    void SpawnGoldIfDead(int damage)
+    {
+        if (magmaBoss)
+        {
+            if (magmaBoss.currentHealth <= damage && GoldBagPrefab != null) //spawn gold
+            {
+                if (OverrideGoldAmount > 0)
+                {
+                    GoldBag bag = Instantiate(GoldBagPrefab, transform.position, Quaternion.identity).GetComponent<GoldBag>();
+                    bag.AmountOfGold = OverrideGoldAmount;
+                }
+                else
+                {
+                    Instantiate(GoldBagPrefab, transform.position, Quaternion.identity);
+                }
+
+            }
+        }
+        if (anchorBoss)
+        {
+            if (anchorBoss.currentHealth <= damage && GoldBagPrefab != null) //spawn gold
+            {
+                if (OverrideGoldAmount > 0)
+                {
+                    GoldBag bag = Instantiate(GoldBagPrefab, transform.position, Quaternion.identity).GetComponent<GoldBag>();
+                    bag.AmountOfGold = OverrideGoldAmount;
+                }
+                else
+                {
+                    Instantiate(GoldBagPrefab, transform.position, Quaternion.identity);
+                }
+
+            }
         }
     }
 }
