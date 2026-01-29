371ec25a5a90ca8932bff6817dc1b97610d77914
Andrewk57 <112024432+Andrewk57@users.noreply.github.com>
2026-01-28 23:03:58 -0500

    side upgrade manager script

---
+++ Assets/sideUpgradeManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class sideUpgradeManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #region Fireball
+    public void increaseFireballRadius()
+    {
+
+    }
+    public void increaseFireballBurnDamage()
+    {
+
+    }
+    public void increaseFireballBurnTime()
+    {
+
+    }
+    public void increaseFireballExplosionDamage()
+    {
+
+    }
+    #endregion
+    #region Electric
+    public void increaseElectricShockDamage()
+    {
+
+    }
+    public void increaseElectricShockRange()
+    {
+
+    }
+    public void increaseKnockBackRange()
+    {
+
+    }
+    public void increaseKnockBackDamage()
+    {
+
+    }
+    #endregion
+    #region curse
+    public void increaseCurseTimeFirst()
+    {
+
+    }
+    public void increaseCurseTimeMax()
+    {
+
+    }
+    public void increasedDamageReflection()
+    {
+
+    }
+    public void increaseSlowEffect()
+    {
+
+    }
+    #endregion
+}
